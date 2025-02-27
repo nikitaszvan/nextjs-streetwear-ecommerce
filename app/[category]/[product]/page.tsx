93fa4554171538783a1ddd53bd5c8bee3cea4b4d
Nikita Van <138067509+nikitaszvan@users.noreply.github.com>
2025-02-26 20:10:33 -0500

    fixed page.tsx for product route

--- app/[category]/[product]/page.tsx
+++ app/[category]/[product]/page.tsx
@@ -1,3 +1,13 @@
-export default function ProductPage() {
-    return null;
-  }
+import ProdRecsWrapper from "@/components/layout/prod-recos-wrapper";
+
+export default async function ProductPage({ params }:
+    {
+        params: Promise<{
+            product: string;
+            category: string;
+        }>
+    }) {
+    const { category, product } = await params;
+
+    return <ProdRecsWrapper category={category} product={product} />;
+}
