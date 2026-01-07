9af7066ef0a470f5a560632b9a4c841363654325
nikitaszvan <nvan3@nait.ca>
2026-01-06 21:05:28 -0500

    migrate dynamodb to upstash redis

--- lib/hooks/use-fetch-products.ts
+++ lib/hooks/use-fetch-products.ts
@@ -1,25 +1,8 @@
-// External Libraries
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
-
 // Data Access Layer
-import { client } from "@/lib/config/dynamodb-config";
-
-const docClient = DynamoDBDocumentClient.from(client);
+import { redis } from '@/lib/config/upstash-vk-config';
 
 export const fetchCategoryProducts = async (pk: string) => {
-  try {
-    const command = new QueryCommand({
-      TableName: "streetwear-ecommerce-imageurls",
-      KeyConditionExpression: "category_pk = :pk",
-      ExpressionAttributeValues: {
-        ":pk": decodeURIComponent(pk)
-      }
-    });
-
-    const response = await docClient.send(command);
-    return response.Items;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-}
+  const categoryKey = decodeURIComponent(pk);
+  const data = await redis.hgetall(categoryKey);
+  return data ? Object.values(data) : [];
+};
